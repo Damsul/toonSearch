0eb508c0528332f6ae1c8a6ec32a070a8e1ca706
Damsul <gyoung9707@naver.com>
2022-10-23 22:31:49 +0900

    toonsearch

---
+++ src/test/java/com/example/toonsearch/ToonSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toonsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToonSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
